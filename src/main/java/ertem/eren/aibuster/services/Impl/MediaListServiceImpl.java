f131d741f22899cdddab9803204e3d6281a0b5e8
Eren Ertem <mail.erenertem@gmail.com>
2025-04-20 12:30:10 +0300

    add services

---
+++ src/main/java/ertem/eren/aibuster/services/Impl/MediaListServiceImpl.java
@@ -0,0 +1,23 @@
+package ertem.eren.aibuster.services.Impl;
+
+import ertem.eren.aibuster.domain.entities.Media;
+import ertem.eren.aibuster.repositories.MediaRepository;
+import ertem.eren.aibuster.services.MediaListService;
+import org.springframework.stereotype.Service;
+import org.w3c.dom.stylesheets.MediaList;
+
+import java.util.List;
+@Service
+public class MediaListServiceImpl implements MediaListService {
+  
+  private final MediaRepository mediaListRepository;
+  
+  public MediaListServiceImpl(MediaRepository mediaListRepository) {
+    this.mediaListRepository = mediaListRepository;
+  }
+  
+  @Override
+  public List<Media> listMediaList() {
+    return mediaListRepository.findAll();
+  }
+}
---
+++ src/main/java/ertem/eren/aibuster/services/MediaListService.java
@@ -0,0 +1,11 @@
+package ertem.eren.aibuster.services;
+
+import ertem.eren.aibuster.domain.entities.Media;
+import org.w3c.dom.stylesheets.MediaList;
+
+import java.util.List;
+
+public interface MediaListService {
+  
+  List<Media> listMediaList();
+}
